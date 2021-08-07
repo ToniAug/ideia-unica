0a3137884acf19ef25f74059f03d02ce24808a39
Toni Augusto <toni.aug@gmail.com>
2021-08-07 20:03:06 -0300

    adicionar cache

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,8 @@
 async function tempo(request, response){
     const dymanicDate = new Date();
 
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
         date: dymanicDate.toGMTString(),
     })
